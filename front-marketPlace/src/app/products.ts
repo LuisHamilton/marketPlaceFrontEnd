e88ec675d32e3b5fcbe3179b81510cac64cef183
LuisHamilton <lhbalem@gmail.com>
2022-05-16 13:11:12 -0300

    adicionando projeto front-end

---
+++ front-marketPlace/src/app/products.ts
@@ -0,0 +1,100 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens'
+    },
+    {
+      id: 2,
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras'
+    },
+    {
+      id: 3,
+      name: 'Phone Standard',
+      price: 299,
+      description: 'rtrtrtrtrtrt'
+    },
+    {
+      id: 4,
+      name: 'Phone Samsung',
+      price: 799,
+      description: 'A large phone with one of the best screens'
+    },
+    {
+      id: 5,
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras'
+    },
+    {
+      id: 6,
+      name: 'Phone Standard',
+      price: 299,
+      description: 'nbnbnbnbnbnbnbnb'
+    },
+    {
+      id: 7,
+      name: 'Phone Standard234',
+      price: 299,
+      description: 'abcdef'
+    },
+    {
+      id: 8,
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens'
+    },
+    {
+      id: 9,
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras'
+    },
+    {
+      id: 10,
+      name: 'Phone Standard',
+      price: 299,
+      description: 'asasassasasas'
+    },
+    {
+      id: 11,
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens'
+    },
+    {
+      id: 12,
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras'
+    },
+    {
+      id: 13,
+      name: 'Phone Standard',
+      price: 299,
+      description: 'oioioioioioi'
+    },
+    {
+      id: 14,
+      name: 'Phone Standard234',
+      price: 299,
+      description: 'abcdef'
+    }
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at https://angular.io/license
+  */
