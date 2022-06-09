4025aebce9e3829bebfa10345b5591f1e3eb94b3
LuisHamilton <lhbalem@gmail.com>
2022-06-09 16:52:16 -0300

    adicionado elemento address-register em client-register e feito a transição

--- front-marketPlace/src/app/address-register/address-register.component.ts
+++ front-marketPlace/src/app/address-register/address-register.component.ts
@@ -11,5 +11,4 @@ export class AddressRegisterComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
--- front-marketPlace/src/app/client-register/client-register.component.ts
+++ front-marketPlace/src/app/client-register/client-register.component.ts
@@ -9,8 +9,59 @@ import axios from 'axios';
 })
 export class ClientRegisterComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  formulario: Boolean | null;
+
+  constructor(private router: Router) {
+    this.formulario = true;
+   }
 
   ngOnInit(): void {
+  }
+
+  continuar(){
+    this.formulario = false;
+  }
+
+  cadastro(){
+    let Name = document.getElementById("name") as HTMLInputElement;
+    let Phone = document.getElementById("phone") as HTMLInputElement;
+    let Email = document.getElementById("e-mail") as HTMLInputElement;
+    let Login = document.getElementById("login") as HTMLInputElement;
+    let Date_of_birth = document.getElementById("date_of_birth") as HTMLInputElement;
+    let Password = document.getElementById("password") as HTMLInputElement;
+    let Document = document.getElementById("document") as HTMLInputElement;
+    let Street = document.getElementById("street") as HTMLInputElement;
+    let City = document.getElementById("city") as HTMLInputElement;
+    let State = document.getElementById("state") as HTMLInputElement;
+    let Country = document.getElementById("country") as HTMLInputElement;
+    let Postal_Code = document.getElementById("postal_code") as HTMLInputElement;
+
+    var data = JSON.stringify({
+      "name" : Name.value,
+      "phone" : Phone.value,
+      "email" : Email.value,
+      "login" : Login.value,
+      "date_of_birth" : Date_of_birth.value,
+      "password" : Password.value,
+      "document" : Document.value,
+      "address" : null
+    });
+
+    var config = {
+      method: 'post',
+      url: 'http://localhost:5151/Client/register',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    };
+
+    let instance = this;
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      instance.router.navigate(['/address-register']);
+    });
   }
 }
