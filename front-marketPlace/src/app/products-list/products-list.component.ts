091795e863deccbf1fefd35919ace16a4b05bf10
LuisHamilton <lhbalem@gmail.com>
2022-07-15 15:04:49 -0300

    pagina addproduct, wishlist view pronta e add wihlist e remove wishlist funcionando

--- front-marketPlace/src/app/products-list/products-list.component.ts
+++ front-marketPlace/src/app/products-list/products-list.component.ts
@@ -8,8 +8,9 @@ import axios from "axios";
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
-  products:  [Product] | undefined
+  products:  Array<Product> = []
   titlePage = "PRODUCTS"
+  produtos: Array<Product> = []
   
   constructor() { }
 
@@ -25,22 +26,90 @@ export class ProductsListComponent implements OnInit {
     axios(config)
     .then(function (response) {
       instance.products = response.data;
+      instance.checar();
     })
     .catch(function (error) {
       console.log(error);
     });
   }
-  mudarCoracao(idTag : String,idStore : Number, idProd : Number){
-    console.log("Selector: "+idTag)        
-    console.log("IdStore: "+idStore) 
-    console.log("IdProduct: "+idProd) 
-    var img = document.querySelector('#'+idTag);           
-    if(img?.getAttribute("src") == "../assets/coracaoFull.png"){
-      img?.setAttribute('src', '../assets/coracao.png');  
+  checar(){
+    for(let produto of this.products){
+      console.log(produto.storeid);
+      var config = {
+        method: 'get',
+        url: 'http://localhost:5151/WishList/verify/' + produto.stocksid,
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem("authToken"),
+          'Content-Type': 'application/json'
+         }
+      };
+      axios(config)
+      .then(function (response) {
+        var img = document.querySelector('#imgCoracao' + produto.id + '_' + produto.storeid);
+        img?.setAttribute('src', '../assets/coracaoFull.png');      
+        console.log(img?.id);
+        
+      })
+      .catch(function (error) {
+        var img = document.querySelector('#imgCoracao' + produto.id + '_' + produto.storeid);
+        img?.setAttribute('src', '../assets/coracao.png');
+      });
     }
-    else{
+  }
+  mudarCoracao(idTag : String,idStore : Number, idProd : Number, idStock : number){
+
+    var config = {
+      method: 'get',
+      url: 'http://localhost:5151/WishList/verify/' + idStock,
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem("authToken"),
+        'Content-Type': 'application/json'
+       }
+    };
+    let instance = this;
+    axios(config)
+    .then(function (response) {
+      instance.remover(idProd, idStock, idTag);
+    })
+    .catch(function (error) {
+      instance.adicionar(idProd, idStock, idTag);
+    });
+  }
+  adicionar(idProd: Number, idStock: number, idTag: String){
+    var img = document.querySelector('#'+idTag);
+    var config = {
+      method: 'post',
+      url: 'http://localhost:5151/WishList/addProduct/' + idProd + '/' + idStock,
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem("authToken"),
+        'Content-Type': 'application/json'
+      }
+    };
+    axios(config)
+    .then(function (response) {
       img?.setAttribute('src', '../assets/coracaoFull.png');
-    }
+    })
+    .catch(function (error) {
+      img?.setAttribute('src', '../assets/coracao.png'); 
+    });
+  }
+  remover(idProd: Number, idStock: number, idTag: String){
+    var img = document.querySelector('#'+idTag);
+    var config = {
+      method: 'delete',
+      url: 'http://localhost:5151/WishList/delete/' + idStock,
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem("authToken"),
+        'Content-Type': 'application/json'
+      }
+    };
+    axios(config)
+    .then(function (response) {
+      img?.setAttribute('src', '../assets/coracao.png'); 
+    })
+    .catch(function (error) {
+      img?.setAttribute('src', '../assets/coracaoFull.png');
+    });
   }
 }
 
